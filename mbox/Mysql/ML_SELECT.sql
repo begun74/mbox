5804a5e88944f8df8cf79eae4a3ffdaa149e47b9
begun74 <begun74@gmail.com>
2015-06-19 13:34:35 +0200

    Signed-off-by: begun74 <begun74@gmail.com>

--- mbox/Mysql/ML_SELECT.sql
+++ mbox/Mysql/ML_SELECT.sql
@@ -1,10 +1,10 @@
-select * from Qmgr where n ='B7AB13B09';
-select * from smtp where n ='B7AB13B09';
-select * from smtpD where n ='B7AB13B09';
+select * from Qmgr where n ='75819132B';
+select * from smtp where n ='75819132B';
+select * from virtual where n ='75819132B';
 
-select distinct q.data,q.from_,s.to_ from Qmgr q 
-inner join smtp s on s.n=q.n
-where q.from_ like 'vit@%';
+select distinct q.data,q.from_,v.to_ from Qmgr q 
+left join virtual v on v.n=q.n
+where q.from_ like '%minsk.gov.by';
 
 select distinct q.data, q.n, s.n, q.from_, s.to_ from Qmgr q 
 inner join smtp s on s.n=q.n
@@ -14,6 +14,6 @@ select distinct q.data, q.n, q.from_, sD.client from Qmgr q
 inner join smtpD sD on sD.n=q.n
 where q.from_ like 'noreply.mosk@%';
 
-select  q.data, q.n, q.from_,v.to_,v.n from Qmgr q 
+select distinct q.data, q.n, q.from_,v.to_,v.n from Qmgr q 
 left join virtual v  on v.n=q.n
-where q.n ='D2CF324E9';
+where q.n ='75819132B';
